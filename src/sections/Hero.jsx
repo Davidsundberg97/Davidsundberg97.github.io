39438fc6c8ae8442c9474e3fdcb5ed76156c2b80
Davidsundberg97 <34091629+Davidsundberg97@users.noreply.github.com>
2024-11-29 19:21:49 +0100

    Fix Hero section with correct scaling and positioning for HackerRoom

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,47 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
+import { Leva, useControls } from 'leva'
 
 const Hero = () => {
+  const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ, scale } = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0,
+      max: 5,
+    },
+  })
+
   return (
     <section className="min-h-screen border-2 w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -13,10 +52,15 @@ const Hero = () => {
         <p className="hero_tag text-gray-500">Software developer</p>
       </div>
       <div className="w-full h-full absolute inset-0">
+        <Leva />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <perspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom scale={0.2} position={[0,0,0]} rotation={[0.25,0,0]}/>
+            <HackerRoom
+              position={[0.0, -0.3, 2.7]}
+              rotation={[0.2, 0.0, 0.0]}
+              scale={[0.15, 0.15, 0.15]}
+            />
             <ambientLight intensity={1} />
             <directionalLight intensity={0.5} position={[10, 10, 10]} />
           </Suspense>
