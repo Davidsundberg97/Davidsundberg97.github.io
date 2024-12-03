34464fbc6ad3c2aabc052958ddcc74a466cb4d8b
Davidsundberg97 <34091629+Davidsundberg97@users.noreply.github.com>
2024-12-03 17:46:01 +0100

    Fixed filepath for imgs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,10 @@
 export const navLinks = [
     { id: 'about', href: '#about', name: 'About' },
-    { id: 'projects', href: '#projects', name: 'Projects' }, // Added Projects link
+    { id: 'projects', href: '#projects', name: 'Projects' },
     { id: 'contact', href: '#contact', name: 'Contact' },
 ];
 
-
-  export const myProjects = [
+export const myProjects = [
     {
       title: 'Particle System - Fire Simulation',
       desc: 'This project is a particle system that simulates fire using Pygame. It allows you to control the size and number of particles using sliders and spawn fire particles by clicking on the screen.',
@@ -18,7 +17,7 @@ export const navLinks = [
         boxShadow: '0px 0px 60px 0px #AA3C304D',
       },
       spotlight: '/assets/spotlight1.png',
-      image: 'public/assets/Fire_sim.PNG', // Added image property
+      image: '/assets/Fire_sim.PNG', // Ensure this path is correct
     },
     {
       title: 'Flask and SQL project for managing data',
@@ -32,11 +31,11 @@ export const navLinks = [
         boxShadow: '0px 0px 60px 0px #3D3E41',
       },
       spotlight: '/assets/spotlight2.png',
-      image: 'public/assets/Flask_project.PNG', // Added image property
+      image: '/assets/Flask_project.PNG', // Ensure this path is correct
     }
-  ];
-  
-  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
     return {
       deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
       deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
@@ -45,9 +44,9 @@ export const navLinks = [
       ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
       targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
     };
-  };
-  
-  export const workExperiences = [
+};
+
+export const workExperiences = [
     {
       id: 1,
       name: 'Framer',
@@ -75,4 +74,4 @@ export const navLinks = [
       icon: '/assets/notion.svg',
       animation: 'salute',
     },
-  ];
+];
